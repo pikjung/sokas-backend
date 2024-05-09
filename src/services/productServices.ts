a3cacfe4d1e02f2ff58a5e6cd40d4c904e075f5b
Fikri <tanjungfikri1@gmail.com>
2024-05-09 11:34:16 +0700

    update 090524

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import xlsx from 'xlsx';
+import xlsx from "xlsx";
 import generateUniqueId from "../utils/generateId";
 
 const prisma = new PrismaClient();
@@ -8,56 +8,49 @@ const productServices = {
   getAllProduct() {
     return prisma.product.findMany({
       include: {
-        Brand: true
-      }
-    })
+        Brand: true,
+      },
+    });
   },
 
   createProduct(data: any) {
-    return prisma.product.create({ data })
+    return prisma.product.create({ data });
   },
 
   updateProduct(id: string, data: any) {
-    return prisma.product.update({ where: { id }, data })
+    return prisma.product.update({ where: { id }, data });
   },
 
   deleteProduct(id: string) {
-    return prisma.product.delete({ where: { id } })
+    return prisma.product.delete({ where: { id } });
   },
 
-  upload(filePath: string) {
+  async upload(filePath: string) {
     try {
       const workbook = xlsx.readFile(filePath);
-      const worksheet = workbook.Sheets['Sheet1'];
+      const worksheet = workbook.Sheets["Sheet1"];
       const data = xlsx.utils.sheet_to_json(worksheet);
 
-      const formattedData = data.map((item: any) => {
-        return {
-          id: generateUniqueId(),
-          name: item.name,
-          value: item.value,
-          brandId: item.brandId,
-        };
-      });
+      const formattedData = data.map((item: any) => ({
+        id: generateUniqueId(),
+        name: item.name,
+        value: item.value,
+        brandId: item.brandId,
+      }));
 
-      const uploadData = prisma.product.create({
-        data: {
-          id: "663b2553.6624dcv",
-          name: "OLD-RC081V LED38S/865 PSU W60L60 GM  MRPCV",
-          value: "AD0311105",
-          brandId: "6639f0bb.bced9xa"
-        },
-        // skipDuplicates: false
-      });
+      const uploadData = formattedData.map((data: any) =>
+        prisma.product.create({ data })
+      );
+      await Promise.all(uploadData);
 
       return {
         success: true,
-        data: uploadData
-      }
+        data: uploadData,
+      };
     } catch (error) {
-      return error
+      return error;
     }
-  }
-}
+  },
+};
 
-export default productServices
+export default productServices;
